44a4469f0b6e836b15aff1e86013c0940ba87ffc
ValValeria <led2017l@gmail.com>
2020-10-25 14:37:56 +0300

    make it better

--- shop/src/app/Components/Header/Header.component.ts
+++ shop/src/app/Components/Header/Header.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector:"header-main",
     templateUrl:"./Header.component.html",
 })
-export class Header{}
+export class Header{
+    @ViewChild('header__links',{read:ElementRef}) links:ElementRef;
+
+    toggleHeader():void{
+        const elem:HTMLUListElement = this.links.nativeElement;
+        
+        this.headerClass(elem)
+
+        window.onresize = ()=>{
+             if(window.innerWidth>770){
+                 elem.classList.remove('none')
+             } else{
+                 elem.classList.add('none')   
+             }
+        }
+    }
+
+    headerClass(elem:HTMLUListElement):void{  
+        
+        if(elem.classList.contains("none")){
+            elem.closest('header').classList.add("header__media");
+        } else{
+            elem.closest('header').classList.remove("header__media");
+        }
+
+        elem.classList.toggle("none");
+    }
+}
