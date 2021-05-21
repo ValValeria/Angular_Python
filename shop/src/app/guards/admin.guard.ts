ab50a0b754d1d290fd0344fffe2e42f4e160d96e
ValValeria <led2017l@gmail.com>
2021-05-21 20:22:22 +0300

    update admin ui

--- shop/src/app/guards/admin.guard.ts
+++ shop/src/app/guards/admin.guard.ts
@@ -1,15 +1,21 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {AuthenticateHelper} from '../Classes/authenticate-helper.service';
 import {User} from '../Services/User.service';
 
 @Injectable()
 export class AdminGuard implements CanActivate{
 
-   constructor(private user: User){}
+   constructor(private user: User,
+               private auth: AuthenticateHelper,
+               private router: Router){}
 
    async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
-     await (new AuthenticateHelper()).authenticate(this.user, true).catch(e => console.log(e));
+     await this.auth.authenticate(this.user, true);
+
+     if (!this.user.is_auth){
+       setTimeout(() => this.router.navigateByUrl('/'));
+     }
 
      return this.user.is_auth;
    }
